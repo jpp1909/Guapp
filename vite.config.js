847ff8ae10ace58f155806fc7b5909e20f61ddbf
jpp1909 <61209016+jpp1909@users.noreply.github.com>
2025-09-29 13:57:19 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://jpp1909.github.io/mi-proyecto-vite"
+  base: './',
 })
